e82c52f38b48982e6e6a8c34e9e70b39dfcdc86c
Aleksandr Savchenko <titamoto@gmail.com>
2023-03-14 11:16:59 -0700

    complete lab

--- index.js
+++ index.js
@@ -1,11 +1,28 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(distance){
+  let bill = "";
+  if (distance <= 400)
+  bill = "This one is on me!";
+  else if (distance > 400 && distance <= 2000)
+  bill = "That will be twenty bucks.";
+  else if (distance > 2000 && distance <= 2500)
+  bill = "I will gladly take your thirty bucks.";
+  else if (distance > 2500) 
+  bill = "No can do.";
+  return bill;
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(city){
+  return city === 'NYC' ? "Ok, sounds good." : "No go.";
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(tip){
+switch (tip) {
+  case "generous" : 
+    return "Thank you so much.";
+  case "not as generous" :
+    return "Thank you.";
+  default:
+    return "Bye."
+
+}
 }
